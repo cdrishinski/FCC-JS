3586c9d4eec224d4d8a9c13a5c449897cbe6b821
cdrishinski <cdrishinski@gmail.com>
2019-08-20 16:51:44 -0600

    sql queries

---
+++ datacamp.sql
@@ -0,0 +1,16 @@
+
+SELECT COUNT(*)
+FROM reviews;
+
+use [AdventureWorks2014]
+
+SELECT * FROM [HumanResources].[Department]
+
+--SHOW ME ALL DEPT NAMES
+
+SELECT Name 
+FROM HumanResources.Department
+
+-- SHOW ME ALL THE GROUPS
+
+SELECT GroupName FROM HumanResources.Department
