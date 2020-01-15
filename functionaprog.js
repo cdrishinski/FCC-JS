2bf7b76132c4f40aaf1aac369eae312f105683d1
cdrishinski <cdrishinski@gmail.com>
2020-01-14 17:41:39 -0700

    more practice

--- functionaprog.js
+++ functionaprog.js
@@ -721,4 +721,15 @@ function urlSlug(title) {
 
 var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
 
+// Functional Programming: Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+function checkPositive(arr) {
+  // Add your code below this line
+  return arr.every(val => val >= 0)
+
+  // Add your code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
+
+
  
