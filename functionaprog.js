e891c2d7bcd3f07455c02b40e5a129d989b36e0f
cdrishinski <cdrishinski@gmail.com>
2020-01-14 17:47:52 -0700

    more practice

--- functionaprog.js
+++ functionaprog.js
@@ -731,5 +731,16 @@ function checkPositive(arr) {
 }
 checkPositive([1, 2, 3, -4, 5]);
 
+// Functional Programming: Use the some Method to Check that Any Elements in an Array Meet a Criteria
+
+function checkPositive(arr) {
+  // Add your code below this line
+  return arr.some(val => val >=0)
+
+  // Add your code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
+
+
 
  
