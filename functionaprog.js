2623a96a4ea343973967978867a9c1be4319fd64
cdrishinski <cdrishinski@gmail.com>
2020-01-14 17:12:25 -0700

    more practice

--- functionaprog.js
+++ functionaprog.js
@@ -662,6 +662,16 @@ function getRating(watchList){
 }
 console.log(getRating(watchList));
 
+// Functional Programming: Sort an Array Alphabetically using the sort Method
+
+function alphabeticalOrder(arr) {
+  // Add your code below this line
+    return arr.sort()
+
+  // Add your code above this line
+}
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+
 
 
  
