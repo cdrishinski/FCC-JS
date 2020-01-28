1135c4a264bab2a31689b06421faca9d590d720c
cdrishinski <cdrishinski@gmail.com>
2020-01-28 09:55:14 -0700

    more practice

--- intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting.js
@@ -69,7 +69,25 @@ function destroyer(arr) {
   
   destroyer([3, 5, 1, 2, 2], 1, 2, 3);
 
+//   Intermediate Algorithm Scripting: Wherefore art thou
+
+function whatIsInAName(collection, source) {
+    // What's in a name?
+    var arr = [];
+    // Only change code below this line
+   var srcKeys = Object.keys(source);
+  
+    return collection.filter(function(obj) {
+      return srcKeys.every(function(key) {
+        return obj.hasOwnProperty(key) && obj[key] === source[key];
+      });
+    });
+  
+    // Only change code above this line
+    return arr;
+  }
+  
+  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
   
 
 
-  
