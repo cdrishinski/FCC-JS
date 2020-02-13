86ccd2179d4712a62a49cc1a8676923a3e00f604
cdrishinski <cdrishinski@gmail.com>
2020-02-12 21:32:53 -0700

    more prac

--- intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting.js
@@ -191,4 +191,25 @@ function spinalCase(str) {
   }
   
   convertHTML("Dolce & Gabbana");
+
+  // Intermediate Algorithm Scripting: Sum All Odd Fibonacci Numbers
+
+  function sumFibs(num) {
+    var prevNumber = 0;
+    var currNumber = 1;
+    var result = 0;
+    while (currNumber <= num) {
+      if (currNumber % 2 !== 0) {
+        result += currNumber;
+      }
+  
+      currNumber += prevNumber;
+      prevNumber = currNumber - prevNumber;
+    }
+  
+    return result;
+  }
+  
+  // test here
+  sumFibs(4);
   
