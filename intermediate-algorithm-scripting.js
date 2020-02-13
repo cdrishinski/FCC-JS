a13868817e2fbe45ceff2a484590f684d7bba2ab
cdrishinski <cdrishinski@gmail.com>
2020-02-12 20:29:17 -0700

    more prac

--- intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting.js
@@ -182,4 +182,13 @@ function spinalCase(str) {
   
   uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
   
+  // Intermediate Algorithm Scripting: Convert HTML Entities
 
+  function convertHTML(str) {
+    // &colon;&rpar;
+   return str.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+  
+  }
+  
+  convertHTML("Dolce & Gabbana");
+  
