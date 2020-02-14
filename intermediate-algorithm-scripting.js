4f698ab28a82b44f9fd06739e93718a1d382af40
cdrishinski <cdrishinski@gmail.com>
2020-02-13 17:25:08 -0700

    more prac

--- intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting.js
@@ -274,6 +274,18 @@ function spinalCase(str) {
   
   // test here
   smallestCommons([1, 5]);
+
+  // Intermediate Algorithm Scripting: Drop it
+
+  function dropElements(arr, func) {
+    return arr.slice(arr.findIndex(func) >= 0 ? arr.findIndex(func) : arr.length);
+  }
+  
+  // test here
+  dropElements([1, 2, 3, 4], function(n) {
+    return n >= 3;
+  });
+
   
 
 
